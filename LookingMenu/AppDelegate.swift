ba9144315da216f4c06b5c02592a497ec732b78a
thanduchuy <thanduchuyz299@gmail.com>
2021-01-04 09:35:34 +0700

    fix conflict

--- LookingMenu/AppDelegate.swift
+++ LookingMenu/AppDelegate.swift
@@ -1,25 +1,12 @@
 import UIKit
-<<<<<<< HEAD
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-
-=======
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
->>>>>>> 4e197549651872e075b73b0bdcdb3de75216e658
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
-<<<<<<< HEAD
-
-}
-=======
 }
 
->>>>>>> 4e197549651872e075b73b0bdcdb3de75216e658
--- LookingMenu/SceneDelegate.swift
+++ LookingMenu/SceneDelegate.swift
@@ -1,8 +1,5 @@
 import UIKit
-<<<<<<< HEAD
-=======
 
->>>>>>> 4e197549651872e075b73b0bdcdb3de75216e658
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     func scene(_ scene: UIScene,
