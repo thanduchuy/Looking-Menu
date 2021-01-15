846c70d6adeea8a0d04be7f9aa95a4b398cae1b2
thanduchuy <thanduchuyz299@gmail.com>
2021-01-15 10:20:21 +0700

    modified Extension

---
+++ LookingMenu/Extensions/UIColor+.swift
@@ -0,0 +1,16 @@
+import Foundation
+import UIKit
+
+extension UIColor {
+    public class var redDesign: UIColor {
+        return UIColor(named: "red") ?? .red
+    }
+    
+    public class var blackDesign: UIColor {
+        return UIColor(named: "black") ?? .black
+    }
+    
+    public class var grayDesign: UIColor {
+        return UIColor(named: "gray") ?? .gray
+    }
+}
---
+++ LookingMenu/Extensions/UIView+.swift
@@ -0,0 +1,17 @@
+import Foundation
+import UIKit
+
+extension UIView {
+    func cornerCircle() {
+        layer.cornerRadius = frame.height/2
+    }
+    
+    func addShadowView(radius: CGFloat) {
+        if let black = UIColor(named: "black") {
+            layer.shadowColor = black.cgColor
+            layer.shadowOpacity = 0.3
+            layer.shadowOffset = .zero
+            layer.shadowRadius = radius
+        }
+    }
+}
